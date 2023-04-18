6bf3a3c1141dab1271430bd405a90281485f1a5b
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-04-18 13:33:33 -0400

    termino del boton editar comienzo del boton Eliminar

--- JS/mascotas.js
+++ JS/mascotas.js
@@ -33,14 +33,15 @@ function listarMascotas (){
     <td>${mascota.dueno}</td>
     <td>
     <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
-        <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-info editar" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-edit"></i></button>
+        <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
     </div>
     </td>
     </tr>`
     ).join(" ")
     listaMascotas.innerHTML = htmlMascotas;
-    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar,index)=>botonEditar.onclick = editar(index))
+    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar,index)=>botonEditar.onclick = editar(index));
+    Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar,index)=>botonEliminar.onclick = eliminar(index));
 }
 
 function enviarDatos(evento){
@@ -50,13 +51,42 @@ function enviarDatos(evento){
         nombre: nombre.value,
         dueno: dueno.value
     };
-    mascotas.push(datos);
+    const accion = botonGuardar.innerHTML;
+    switch(accion){
+        case 'Editar':
+            mascotas[indice.value] = datos;
+            break;
+        default: 
+            mascotas.push(datos);
+            break
+    }   
     listarMascotas();
+    resetModal();
 }
 
-function editar(indice){
+function editar(index){
+    botonGuardar.innerHTML = 'Editar'
+    $('#exampleModal').modal('toggle');
     return function cuandoHagoClick(){
-        console.log(indice);
+        const mascota = mascotas[index];
+        tipo.value = mascota.tipo;
+        nombre.value = mascota.nombre;
+        dueno.value = mascota.dueno;
+        indice.value = index;
+    }
+}
+
+function resetModal(){
+    tipo.value = "";
+    nombre.value = "";
+    dueno.value = "";
+    indice.value = "";
+    botonGuardar.innerHTML = 'Crear';
+}
+
+function eliminar(index){
+    return function (clickEnEliminar){
+        console.log('index',index);
     }
 }
 
