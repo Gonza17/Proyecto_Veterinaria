1a3f914830d4c73667298a18724a3133bb1ae834
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-05-22 16:30:56 -0400

    cambios de los recursos y dentro del enrutador

--- node/enrutador.js
+++ node/enrutador.js
@@ -1,10 +1,12 @@
 const recursos = require("./recursos");
 const mascotas = require("./rutas/mascotas");
+const veterinarias = require("./rutas/veterinarias");
 module.exports = {
     ruta: (data, callback) => {
         callback(200, { mensaje: 'Esta es /ruta' });
     },
     mascotas: mascotas(recursos.mascotas),
+    veterinarias: veterinarias(recursos.veterinarias),
     noEncontrado: (data, callback) => {
         callback(404, { mensaje: 'No encontrado' })
     },
--- node/recursos.js
+++ node/recursos.js
@@ -5,5 +5,10 @@ module.exports = {
         { tipo: "Gato", nombre: "Martín", dueno: "Alejandro" },
         { tipo: "Gato", nombre: "Trinity", dueno: "Vicente" },
     ],
-
+    veterinarias: [
+        { nombre: "Alexandra", apellido: "Perez", documneto: "222333455543" },
+        { nombre: "Alexander", apellido: "Gómez", documneto: "839874191928" },
+        { nombre: "Maricel", apellido: "Gonzalez", documneto: "123712938712" },
+        { nombre: "Celicia", apellido: "Rocco", documneto: "12398271398123" },
+    ],
 };
