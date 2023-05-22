f266a5dca387f8c33dea2e806c8127c8d5efb588
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-05-22 16:07:46 -0400

    refactor de las rutas

--- node/enrutador.js
+++ node/enrutador.js
@@ -1,43 +1,10 @@
+const recursos = require("./recursos");
+const mascotas = require("./rutas/mascotas");
 module.exports = {
     ruta: (data, callback) => {
         callback(200, { mensaje: 'Esta es /ruta' });
     },
-    mascotas: {
-        get: (data, callback) => {
-            if (typeof data.indice !== "undefined") {
-                if (global.recursos.mascotas[data.indice]) {
-                    return callback(200, global.recursos.mascotas[data.indice])
-                }
-                return callback(404, { mensaje: `mascota con indice: ${data.indice} no encontrada` })
-            }
-            callback(200, global.recursos.mascotas);
-        },
-        post: (data, callback) => {
-            console.log("handler: ", { data });
-            global.recursos.mascotas.push(data.payload);
-            callback(201, data.payload);
-        },
-        put: (data, callback) => {
-            if (typeof data.indice !== "undefined") {
-                if (global.recursos.mascotas[data.indice]) {
-                    global.recursos.mascotas[data.indice] = data.payload;
-                    return callback(200, global.recursos.mascotas[data.indice]);
-                }
-                return callback(404, { mensaje: `mascota con indice ${data.indice} no encontrada` });
-            }
-            callback(400, { mensaje: "indice no enviado" });
-        },
-        delete: (data, callback) => {
-            if (typeof data.indice !== "undefined") {
-                if (global.recursos.mascotas[data.indice]) {
-                    global.recursos.mascotas = global.recursos.mascotas.filter((_mascota, indice) => indice != data.indice);
-                    return callback(204, { mensaje: `Elemento del indice ${data.indice} eliminado`, });
-                }
-                return callback(404, { mensaje: `mascota con indice ${data.indice} no encontrada` });
-            }
-            callback(400, { mensaje: "indice no enviado" });
-        },
-    },
+    mascotas: mascotas(recursos.mascotas),
     noEncontrado: (data, callback) => {
         callback(404, { mensaje: 'No encontrado' })
     },
---
+++ node/rutas/mascotas.js
@@ -0,0 +1,39 @@
+module.exports = function mascotasHandler(mascotas) {
+    return {
+        get: (data, callback) => {
+            if (typeof data.indice !== "undefined") {
+                if (mascotas[data.indice]) {
+                    return callback(200, mascotas[data.indice])
+                }
+                return callback(404, { mensaje: `mascota con indice: ${data.indice} no encontrada` })
+            }
+            callback(200, mascotas);
+        },
+        post: (data, callback) => {
+            console.log("handler: ", { data });
+            mascotas.push(data.payload);
+            callback(201, data.payload);
+        },
+        put: (data, callback) => {
+            if (typeof data.indice !== "undefined") {
+                if (mascotas[data.indice]) {
+                    mascotas[data.indice] = data.payload;
+                    return callback(200, mascotas[data.indice]);
+                }
+                return callback(404, { mensaje: `mascota con indice ${data.indice} no encontrada` });
+            }
+            callback(400, { mensaje: "indice no enviado" });
+        },
+        delete: (data, callback) => {
+            if (typeof data.indice !== "undefined") {
+                if (mascotas[data.indice]) {
+                    mascotas = mascotas.filter((_mascota, indice) => indice != data.indice);
+                    return callback(204, { mensaje: `Elemento del indice ${data.indice} eliminado`, });
+                }
+                return callback(404, { mensaje: `mascota con indice ${data.indice} no encontrada` });
+            }
+            callback(400, { mensaje: "indice no enviado" });
+        },
+    };
+}
+
